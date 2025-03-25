ebd436de61a66be4f5ed2557a8b2b3f546482020
christiaanmealey <christiaan.mealey@gmail.com>
2025-03-25 12:55:05 -0400

    fix

--- src/utils/awsConfig.ts
+++ src/utils/awsConfig.ts
@@ -1,6 +1,5 @@
 import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
 import { DynamoDBDocumentClient } from '@aws-sdk/lib-dynamodb';
-import { CognitoIdentityClient } from "@aws-sdk/client-cognito-identity";
 import { fromCognitoIdentityPool } from "@aws-sdk/credential-providers";
 
 const REGION = "us-east-2"; 
@@ -9,8 +8,8 @@ const IDENTITY_POOL_ID = "us-east-2:9efe3453-d876-4532-aeb6-80a83dd12072"
 const client = new DynamoDBClient({
   region: REGION,
   credentials: fromCognitoIdentityPool({
-    client: new CognitoIdentityClient({ region: REGION }),
     identityPoolId: IDENTITY_POOL_ID,
+    clientConfig: { region: REGION }
   }),
 });
 
