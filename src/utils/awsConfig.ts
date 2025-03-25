13c996dd21f843fa04d385a75667162b179e2f54
christiaanmealey <christiaan.mealey@gmail.com>
2025-03-25 12:30:04 -0400

    switching to cognito identity provider

--- src/utils/awsConfig.ts
+++ src/utils/awsConfig.ts
@@ -1,12 +1,17 @@
 import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
 import { DynamoDBDocumentClient } from '@aws-sdk/lib-dynamodb';
-import { fromEnv } from "@aws-sdk/credential-providers";
+import { CognitoIdentityClient } from "@aws-sdk/client-cognito-identity";
+import { fromCognitoIdentityPool } from "@aws-sdk/credential-providers";
 
-const REGION = "us-east-2"; // Change to your region
+const REGION = "us-east-2"; 
+const IDENTITY_POOL_ID = "us-east-2:9efe3453-d876-4532-aeb6-80a83dd12072"
 
 const client = new DynamoDBClient({
   region: REGION,
-  credentials: fromEnv()
+  credentials: fromCognitoIdentityPool({
+    client: new CognitoIdentityClient({ region: REGION }),
+    identityPoolId: IDENTITY_POOL_ID,
+  }),
 });
 
 const docClient = DynamoDBDocumentClient.from(client);
