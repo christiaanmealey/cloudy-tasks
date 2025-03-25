3edf24c3610e7be08a7ff47a0d40801254551f30
christiaanmealey <christiaan.mealey@gmail.com>
2025-03-25 11:29:47 -0400

    adding fromEnv creds

--- src/utils/awsConfig.ts
+++ src/utils/awsConfig.ts
@@ -1,8 +1,12 @@
 import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
 import { DynamoDBDocumentClient } from '@aws-sdk/lib-dynamodb';
+import { fromEnv } from "@aws-sdk/credential-providers";
+
+const REGION = "us-east-2"; // Change to your region
 
 const client = new DynamoDBClient({
-    region: 'us-east-2'
+  region: REGION,
+  credentials: fromEnv()
 });
 
 const docClient = DynamoDBDocumentClient.from(client);
